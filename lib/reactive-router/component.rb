258990ec055b1df68a72fdd7cd11a85ad039d331
Mitch VanDuyn <mitch@catprint.com>
2015-09-10 21:13:01 -0400

    initial commit

--- lib/reactive-router/component.rb
+++ lib/reactive-router/component.rb
@@ -2,27 +2,40 @@ module React
   module Component
 
     module ClassMethods
-
-      def router_param(name, &block)
-        define_state name
-        before_mount do
-          send("#{name}!", yield(params[:params][name]))
-        end
-        before_receive_props do |new_params|
-          send("#{name}!", yield(new_params[:params][name]))
-        end
+      
+      def url_param_evaluators
+        @url_param_evaluators ||= {}
       end
-
+      
+      attr_accessor :evaluated_url_params
+      
+      def router_param(name, &block)
+        
+        url_param_evaluators[name] = block
+        
+        class << self
+          
+          define_method name do
+            evaluated_url_params[name]
+          end
+        end
+        
+        define_method name do
+          self.class.send(name)
+        end
+        
+      end
+      
     end
 
-    def route_handler
-      Router::RR::RouteHandler()
+    def route_handler(*args)
+      Router::RR::RouteHandler(*args)
     end
 
     def link(opts = {}, &block)
       opts[:params] = opts[:params].to_n if opts[:params]
       Router::RR::Link(opts, &block)
     end
-
+    
   end
 end
--- lib/reactive-router/router.rb
+++ lib/reactive-router/router.rb
@@ -13,8 +13,41 @@ module React
         include React::Component
         include React::IsomorphicHelpers
         
+        native_mixin `ReactRouter.Navigation`
+        native_mixin `ReactRouter.State`
+        
+        def get_path
+          path = `self.native.getPath()`
+          path = nil if `typeof path === 'undefined'`
+          path
+        end
+        
+        def replace_with(route_or_path, params = nil, query = nil)
+          `self.native.replaceWith.apply(self.native, #{[route_or_path, params].compact})`
+        end
+        
+        def transition_to(route_or_path, params = nil, query = nil)
+          `self.native.transitionTo.apply(self.native, #{[route_or_path, params].compact})`
+        end
+        
+        static_call_back "willTransitionTo" do |transition, params, query, callback|
+          if self.respond_to? :will_transition_to
+            result = will_transition_to transition, params, query if self.respond_to? :will_transition_to
+            if result.is_a? Promise
+              result.then { |r| callback(r) }
+            else
+              callback.call()
+            end
+          else
+            callback.call()
+          end
+        end
+        
         before_first_mount do |context|
-          if `typeof ReactRouter === 'undefined'`
+          @evaluated_url_params = {}
+          if !self.instance_methods.include?(:show)  # if there is no show method this is NOT a top level router so we assume routing will begin elsewhere
+            @routing[ = true
+          elsif `typeof ReactRouter === 'undefined'`
             if on_opal_client?
               message = "ReactRouter not defined in browser assets - you must manually include it in your assets"
             else
@@ -28,15 +61,44 @@ module React
         end
 
         export_component
+        
+        optional_param :router_state # optional because it is not initially passed in but we add it when running the router
+        #optional_param :query
+        #optional_param :params
+        
+        def url_params(params)
+          params[:params] || (params[:router_state] && params[:router_state][:params]) || {}
+        end
+        
+        class << self
+
+        end
+
+        before_mount do
+          url_params(params).each do |param, value| 
+            if evaluator = self.class.url_param_evaluators[param]
+              self.class.evaluated_url_params[param] = evaluator.call(value)
+            end
+          end
+        end
+        
+        before_receive_props do |params|
+          url_params(params).each do |param, value| 
+            if evaluator = self.class.url_param_evaluators[param]
+              self.class.evaluated_url_params[param] = evaluator.call(value) 
+            end
+          end
+        end
 
         def render
           if self.class.routing?
             show
           elsif on_opal_server?
             self.class.routing!
-            routes = self.class.build_routes
+            routes = self.class.build_routes(true)
             %x{
               ReactRouter.run(#{routes}, window.reactive_router_static_location, function(root) {
+                self.native.props.router_state = state
                 self.root = React.createElement(root, self.native.props);
               });
             }
@@ -61,9 +123,10 @@ module React
             else
               `React.findDOMNode(#{self}.native)`    # v0.13.0
             end
-            routes = self.class.build_routes
+            routes = self.class.build_routes(true)
             %x{
-              ReactRouter.run(#{routes}, ReactRouter.HistoryLocation, function(root) {
+              ReactRouter.run(#{routes}, ReactRouter.HashLocation, function(root, state) {
+                self.native.props.router_state = state
                 React.render(React.createElement(root, self.native.props), #{dom_node});
               });
             }
@@ -75,13 +138,19 @@ module React
           @routes_block = block
         end
         
-        def self.build_routes
+        def self.routes_block
+          @routes_block
+        end
+        
+        def self.build_routes(generate_node = nil)
+          #raise "You must define a routes block in a router component" unless @routes_block
           routes_opts = @routes_opts.dup
           routes_opts[:handler] ||= self 
-          route(routes_opts, generate_node = true, &@routes_block)
+          route(routes_opts, generate_node, &@routes_block)
         end
 
         def self.route(opts = {}, generate_node = nil, &block)
+          block ||= opts[:handler].routes_block if opts[:handler].respond_to? :routes_block
           opts = opts.dup
           opts[:handler] = React::API.create_native_react_class(opts[:handler])
           (generate_node ? RR::Route_as_node(opts, &block) : RR::Route(opts, &block)) 
@@ -93,6 +162,11 @@ module React
 
         def self.redirect(opts = {}, &block)
           RR::Redirect(opts, &block)
+        end
+        
+        def self.not_found(opts={}, &block)
+          opts[:handler] = React::API.create_native_react_class(opts[:handler])
+          RR::NotFoundRoute(opts, &block)
         end
 
       end
