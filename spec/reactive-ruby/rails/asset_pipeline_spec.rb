a194644ecdbf20252b6e88fa6cc8c350cde4bb65
David Chang <zeta11235813@gmail.com>
2016-09-24 19:22:39 +0800

    Pass parent BUNDLE_GEMFILE when shell out

--- spec/reactive-ruby/rails/asset_pipeline_spec.rb
+++ spec/reactive-ruby/rails/asset_pipeline_spec.rb
@@ -3,7 +3,8 @@ require 'spec_helper'
 if ruby?
 RSpec.describe 'test_app generator' do
   it "does not interfer with asset precompilation" do
-    expect(system("cd spec/test_app; bundle exec rake assets:precompile")).to be_truthy
+    env = { "BUNDLE_GEMFILE" => ENV['BUNDLE_GEMFILE'] }
+    expect(system(env, "cd spec/test_app; bundle exec rake assets:precompile")).to be_truthy
   end
 end
 end
