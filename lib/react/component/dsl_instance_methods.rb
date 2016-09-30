d6fce165244a71b54fe7116c0d7cb55b4a0d745c
David Chang <zeta11235813@gmail.com>
2016-09-30 15:34:45 +0800

    Fix missing requires & move React::Children back

--- lib/react/component/dsl_instance_methods.rb
+++ lib/react/component/dsl_instance_methods.rb
@@ -1,40 +1,10 @@
+require "react/children"
+
 module React
   module Component
     module DslInstanceMethods
       def children
-        if `#{@native}.props.children==undefined`
-          nodes = []
-        else
-          nodes = [`#{@native}.props.children`].flatten
-        end
-        class << nodes
-          include Enumerable
-
-          def to_n
-            self
-          end
-
-          def each(&block)
-            if block_given?
-              %x{
-                    React.Children.forEach(#{self.to_n}, function(context){
-              #{yield React::Element.new(`context`)}
-                    })
-              }
-              nil
-            else
-              Enumerator.new(`React.Children.count(#{self.to_n})`) do |y|
-                %x{
-                      React.Children.forEach(#{self.to_n}, function(context){
-                #{y << Element.new(`context`)}
-                      })
-                }
-              end
-            end
-          end
-        end
-
-        nodes
+        Children.new(`#{@native}.props.children`)
       end
 
       def params
