cdf79483ce9b314af6ffb2a49d6968ef1e1cbc4d
David Chang <zeta11235813@gmail.com>
2016-10-13 13:29:47 +0800

    Update exception when React.js is not defined

--- lib/reactive-ruby.rb
+++ lib/reactive-ruby.rb
@@ -5,17 +5,12 @@ if RUBY_ENGINE == 'opal'
     raise [
       "No React.js Available",
       "",
-      "React.js must be defined before requiring 'reactive-ruby'",
-      "'reactive-ruby' has been tested with react v13, v14, and v15.",
+      "A global `React` must be defined before requiring 'reactive-ruby'",
       "",
-      "IF USING 'react-rails':",
-      "   add 'require \"react\"' immediately before the 'require \"reactive-ruby\" directive in 'views/components.rb'.",
+      "To USE THE BUILT-IN SOURCE: ",
+      "   add 'require \"react/react-source\"' immediately before the 'require \"reactive-ruby\" directive.",
       "IF USING WEBPACK:",
-      "   add 'react' to your webpack manifest.",
-      "OTHERWISE TO GET THE LATEST TESTED VERSION",
-      "   add 'require \"react-latest\"' immediately before the require of 'reactive-ruby',",
-      "OR TO USE A SPECIFIC VERSION",
-      "   add 'require \"react-v1x\"' immediately before the require of 'reactive-ruby'."
+      "   add 'react' to your webpack manifest."
     ].join("\n")
   end
   require 'react/top_level'
