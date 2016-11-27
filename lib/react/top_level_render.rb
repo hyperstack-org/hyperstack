fab41248fd6a6b81c370d48b34ba32015b03383a
David Chang <zeta11235813@gmail.com>
2016-11-27 18:32:29 +0800

    Fix unmount_component_at_node test

--- lib/react/top_level_render.rb
+++ lib/react/top_level_render.rb
@@ -1,17 +1,26 @@
 module React
   def self.render(element, container)
     container = `container.$$class ? container[0] : container`
+
+    cb = %x{
+      function(){
+        setTimeout(function(){
+          #{yield if block_given?}
+        }, 0)
+      }
+    }
+
     if !(`typeof ReactDOM === 'undefined'`)
-      native = `ReactDOM.render(#{element.to_n}, container, function(){#{yield if block_given?}})` # v0.15+
+      native = `ReactDOM.render(#{element.to_n}, container, cb)` # v0.15+
     elsif !(`typeof React.renderToString === 'undefined'`)
-      native = `React.render(#{element.to_n}, container, function(){#{yield if block_given?}})`
+      native = `React.render(#{element.to_n}, container, cb)`
     else
       raise "render is not defined.  In React >= v15 you must import it with ReactDOM"
     end
 
     if `#{native}._getOpalInstance !== undefined`
       `#{native}._getOpalInstance()`
-    elsif `React.findDOMNode !== undefined && #{native}.nodeType == undefined`
+    elsif `React.findDOMNode !== undefined && #{native}.nodeType === undefined`
       `React.findDOMNode(#{native})`
     else
       native
