e75abf4572125fa84a6170504e9010b923728a2b
catmando <mitch@catprint.com>
2017-03-24 02:50:34 -0400

    force load opal browser first

--- hyperloop-config.gemspec
+++ hyperloop-config.gemspec
@@ -18,6 +18,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency 'opal'
+  spec.add_dependency 'opal-browser'
 
   spec.add_development_dependency "bundler", "~> 1.12"
   spec.add_development_dependency "rake", "~> 10.0"
--- lib/hyperloop-config.rb
+++ lib/hyperloop-config.rb
@@ -9,6 +9,7 @@ else
   require 'hyperloop/on_client'
   require 'hyperloop/rail_tie' if defined? Rails
   Hyperloop.import 'opal', gem: true
+  Hyperloop.import 'browser', client_only: true
   Hyperloop.import 'hyperloop-config', gem: true
   Opal.append_path(File.expand_path('../', __FILE__).untaint)
 end
