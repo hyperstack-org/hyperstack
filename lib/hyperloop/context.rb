b38cbf29120bb49b3c86b2a3043fa4bd027ff2b9
catmando <mitch@catprint.com>
2017-03-28 12:32:40 -0400

    more flexibility with Context.reset

--- lib/hyperloop/context.rb
+++ lib/hyperloop/context.rb
@@ -5,26 +5,32 @@ module Hyperloop
   # the provided block will be rerun and the instance variable re-initialized
   # when the reset! method is called
   module Context
-    # pass self, an instance var name (symbol) and a block
-    # if reset! has been called, then record the var and block for future resets
-    # then if var is currently empty it will be initialized with block
-    def self.set_var(ctx, var, &block)
-      @context_hash[ctx] ||= [var, block] if @context_hash
-      ctx.instance_variable_get(var) || ctx.instance_variable_set(var, yield)
+    # Replace @foo ||= ... with
+    # Context.set_var(self, :@foo) { ... }
+    # If reset! has been called then the instance variable will be record, and
+    # will be reset on the next call to reset!
+    # If you want to record the current value of the instance variable then set
+    # force to true.
+    def self.set_var(ctx, var, force: nil)
+      puts "calling set_var(#{var})"
+      inst_value_b4 = ctx.instance_variable_get(var)
+      if @context && !@context[ctx].key?(var) && (force || !inst_value_b4)
+        @context[ctx][var] = (inst_value_b4 && inst_value_b4.dup)
+      end
+      inst_value_b4 || ctx.instance_variable_set(var, yield)
     end
 
     def self.reset!(reboot = true)
-      # if @context_hash is already initialized then reset all the instance
+      # if @context is already initialized then reset all the instance
       # vars using their corresponding blocks.  Otherwise initialize
-      # @context_hash.
-      if @context_hash
-        @context_hash.each do |context, var_and_block|
-          var, block = var_and_block
-          context.instance_variable_set(var, block.call)
+      # @context.
+      if @context
+        @context.each do |ctx, vars|
+          vars.each { |var, init| ctx.instance_variable_set(var, init) }
         end
         Hyperloop::Application::Boot.run if reboot
       else
-        @context_hash = {}
+        @context = Hash.new { |h, k| h[k] = {} }
       end
     end
   end
