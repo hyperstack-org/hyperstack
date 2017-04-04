e6dde3a513ece0d4b618907aac7698d2490ffce6
catmando <mitch@catprint.com>
2017-04-03 23:56:39 -0400

    auto compile and compression

--- lib/hyperloop/imports.rb
+++ lib/hyperloop/imports.rb
@@ -36,17 +36,18 @@ module Hyperloop
       import(nil, instead_of: value)
     end
 
-    def generate_requires(mode, file)
+    def generate_requires(mode, sys, file)
       puts "***** generating requires for #{mode} - #{file}"
       import_list.collect do |value, render_on_server, render_on_client, kind|
         next unless value
+        next if (sys && kind == :tree) || (!sys && kind != :tree)
         next if mode == :client && !render_on_client
         next if mode == :server && !render_on_server
         if kind == :tree
           generate_require_tree(value, render_on_server, render_on_client)
         elsif kind == :gem
           r = "require '#{value}' #{client_guard(render_on_server, render_on_client)}"
-          puts r
+          puts "    #{r}"
           "puts \"#{r}\"; #{r}"
         else
           generate_directive(:require, value, file, render_on_server, render_on_client)
@@ -62,7 +63,7 @@ module Hyperloop
         comp_path.shift
       end
       r = "#{directive} '#{(['.'] + ['..'] * gem_path.length + comp_path).join('/')}' #{client_guard(render_on_server, render_on_client)}"
-      puts r
+      puts "    #{r}"
       "puts \"#{r}\"; #{r}"
     end
 
@@ -74,7 +75,7 @@ module Hyperloop
         if fname =~ /(\.js$)|(\.rb$)/
           fname = fname.gsub(/(\.js$)|(\.rb$)/, '')
           r = "require '#{fname}' #{client_guard(render_on_server, render_on_client)}"
-          puts r
+          puts "    #{r}"
           "puts \"#{r}\"; #{r}"
         end
       end.compact.join("\n")
@@ -86,6 +87,19 @@ module Hyperloop
       elsif !render_on_client
         '# SERVER ONLY'
       end
+    end
+
+    def compile_and_compress(name)
+      start_time = Time.now
+      puts "Compiling the system assets for #{name}"
+      compiled_code = Rails.application.assets[name].to_s
+      compilation_time = Time.now
+      puts "  compiled -  length: #{compiled_code.length}"
+      compressed_code = Uglifier.new.compile(compiled_code)
+      puts "  minimized - length: #{compressed_code.length}"
+      puts "  minification ratio #{(compressed_code.length*100.0/compiled_code.length).round}%"
+      puts "  total time: #{(Time.now-start_time).to_f.round(2)} seconds"
+      compressed_code
     end
 
   end
