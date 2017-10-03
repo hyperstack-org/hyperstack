ca1ca35912ad09859a2ebc9024549ef98ea5f4d4
Jan Biedermann <jan@kursator.de>
2017-10-03 21:51:32 +0200

    fix assets config SUCKER!

--- spec/test_app/config/initializers/assets.rb
+++ spec/test_app/config/initializers/assets.rb
@@ -3,7 +3,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.paths << ::Rails.root.join('app', 'views')
-Rails.application.config.assets.precompile += [/.*\.js/,/.*\.css/]
+Rails.application.config.assets.precompile += %w[jquery_ujs.js jquery.js time_cop.js factorial.js test.css]
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
