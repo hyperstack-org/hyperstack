7ea511ad301209f00cd33af2a5b0d02ac34e5f17
Jan Biedermann <jan@kursator.de>
2017-10-08 14:32:09 +0200

    load and start autoloader properly

--- lib/hyperloop-config.rb
+++ lib/hyperloop-config.rb
@@ -14,21 +14,7 @@ else
   Hyperloop.import 'browser', client_only: true
   Hyperloop.import 'hyperloop-config', gem: true
   Hyperloop.import 'hyperloop/autoloader'
-  Hyperloop::Autoloader.load_paths = %w[components models operations stores]
-
-  class Object
-    class << self
-      alias _autoloader_original_const_missing const_missing
-
-      def const_missing(const_name)
-        # need to call original code because some things are set up there
-        # original code may also be overloaded by reactrb, for example
-        _autoloader_original_const_missing(const_name)
-      rescue StandardError => e
-        Hyperloop::Autoloader.const_missing(const_name, self) || raise(e)
-      end
-    end
-  end
+  Hyperloop.import 'hyperloop/autoloader_starter'
 
   Opal.append_path(File.expand_path('../', __FILE__).untaint)
 end
