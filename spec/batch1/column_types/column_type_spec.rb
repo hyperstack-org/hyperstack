13d52e4b6af02df4c70fcaf6658e67a3bd0710ec
Jan Biedermann <jan@kursator.de>
2017-11-01 14:05:19 +0100

    add default_test model

--- spec/batch1/column_types/column_type_spec.rb
+++ spec/batch1/column_types/column_type_spec.rb
@@ -65,16 +65,6 @@ describe "column types on client", js: true do
       config.opts = {app_id: Pusher.app_id, key: Pusher.key, secret: Pusher.secret}.merge(PusherFake.configuration.web_options)
     end
 
-    class DefaultTest < ActiveRecord::Base
-      def self.build_tables
-        connection.create_table :default_tests, force: true do |t|
-          t.string :string, default: "I'm a string!"
-          t.date :date, default: Date.today
-          t.datetime :datetime, default: Time.now
-        end
-      end
-    end
-
     isomorphic do
       class TypeTest < ActiveRecord::Base
         server_method :a_server_method, default: "hello" do |s|
