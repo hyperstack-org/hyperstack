c8c72d17cde9321268269dddad2043fb51b1bab1
Jan Biedermann <jan@kursator.de>
2017-12-08 20:16:24 +0100

    make HAC the default for prerendering

--- lib/reactive-ruby/rails/railtie.rb
+++ lib/reactive-ruby/rails/railtie.rb
@@ -4,10 +4,9 @@ module ReactiveRuby
       config.before_configuration do |app|
         app.config.assets.enabled = true
         app.config.assets.paths << ::Rails.root.join('app', 'views').to_s
-        app.config.react.server_renderer =
-          ReactiveRuby::ServerRendering::ContextualRenderer
-        app.config.react.view_helper_implementation =
-          ReactiveRuby::Rails::ComponentMount
+        app.config.react.server_renderer = ReactiveRuby::ServerRendering::ContextualRenderer
+        app.config.react.view_helper_implementation = ReactiveRuby::Rails::ComponentMount
+        ReactiveRuby::ServerRendering::ContextualRenderer.asset_container_class = ReactiveRuby::ServerRendering::HyperAssetContainer
       end
       config.after_initialize do
         class ::HyperloopController < ::ApplicationController
