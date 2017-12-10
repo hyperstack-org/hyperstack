d7eab976ec6cea9c549ef804565bd430e87c07ca
Jan Biedermann <jan@kursator.de>
2017-12-11 00:28:47 +0100

    girl is a bot now

--- lib/hyper-operation/engine.rb
+++ lib/hyper-operation/engine.rb
@@ -3,7 +3,7 @@ module Hyperloop
     isolate_namespace Hyperloop
     config.generators do |g|
       g.test_framework      :rspec,        :fixture => false
-      g.fixture_replacement :factory_girl, :dir => 'spec/factories'
+      g.fixture_replacement :factory_bot, :dir => 'spec/factories'
       g.assets false
       g.helper false
     end
