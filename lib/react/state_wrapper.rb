c9e5becd6821271b2ba58e936bf49ceb367789e6
Jan Biedermann <jan@kursator.de>
2018-01-05 18:00:29 +0100

    small performance improvement

--- lib/react/state_wrapper.rb
+++ lib/react/state_wrapper.rb
@@ -12,9 +12,9 @@ module HyperStore
     alias pre_component_method_missing method_missing
 
     def method_missing(method, *args)
-      if method =~ /\!$/ && __from__.respond_to?(:deprecation_warning)
-        __from__.deprecation_warning("The mutator 'state.#{method}' has been deprecated.  Use 'mutate.#{method.gsub(/\!$/,'')}' instead.")
-        __from__.mutate.__send__(method.gsub(/\!$/,''), *args)
+      if method.end_with?('!') && __from__.respond_to?(:deprecation_warning)
+        __from__.deprecation_warning("The mutator 'state.#{method}' has been deprecated.  Use 'mutate.#{method.sub(/\!$/,'')}' instead.")
+        __from__.mutate.__send__(method.chop, *args)
       else
         pre_component_method_missing(method, *args)
       end
