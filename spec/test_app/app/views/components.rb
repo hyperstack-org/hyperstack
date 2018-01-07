d95748ecd91823d14d93b2f7b3326dfa044c7cad
Jan Biedermann <jan@kursator.de>
2018-01-08 00:00:46 +0100

    proper require order

--- spec/test_app/app/views/components.rb
+++ spec/test_app/app/views/components.rb
@@ -1,10 +1,13 @@
 require 'opal'
 require 'react/react-source-browser'
 require 'hyper-component'
-require 'hyper-operation'
 if React::IsomorphicHelpers.on_opal_client?
+  require 'browser/support'
+  require 'browser/event'
+  require 'browser/window'
   require 'browser/delay'
   require 'browser/interval'
   require 'hyperloop/pusher'
 end
+require 'hyper-operation'
 require_tree './components'
