3fcab7e247c64cd6c5df80cadfc32b9839529d54
Jan Biedermann <jan@kursator.de>
2018-01-08 00:44:54 +0100

    remove jquery

--- spec/test_app/config/initializers/assets.rb
+++ spec/test_app/config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w[jquery.js jquery_ujs.js time_cop.js]
+Rails.application.config.assets.precompile += %w[time_cop.js]
