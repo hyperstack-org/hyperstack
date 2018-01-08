e914079c68d2ff18e86effec908e7f7b5c18b92a
Jan Biedermann <jan@kursator.de>
2018-01-08 11:04:13 +0100

    refinement

--- spec/test_app/app/views/components.rb
+++ spec/test_app/app/views/components.rb
@@ -2,9 +2,7 @@ require 'opal'
 require 'promise'
 require 'hyper-react'
 if React::IsomorphicHelpers.on_opal_client?
-  require 'browser/support'
-  require 'browser/event'
-  require 'browser/window'
+  require 'browser'
   require 'browser/delay'
 end
 require_tree './components'
