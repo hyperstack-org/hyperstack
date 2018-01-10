6d596e3e935b0ea0e77de3391994b931633d7516
Jan Biedermann <jan@kursator.de>
2018-01-10 10:52:09 +0100

    convert to hyper-spec

--- spec/react/callbacks_spec.rb
+++ spec/react/callbacks_spec.rb
@@ -1,138 +1,142 @@
 require 'spec_helper'
 
-if opal?
-describe React::Callbacks do
+describe 'React::Callbacks', js: true do
   it 'defines callback' do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Callbacks
-      define_callback :before_dinner
-      before_dinner :wash_hand
+    on_client do
+      class Foo
+        include React::Callbacks
+        define_callback :before_dinner
+        before_dinner :wash_hands
 
-      def wash_hand
+        def wash_hands;end
       end
     end
 
-    instance = Foo.new
-    expect(instance).to receive(:wash_hand)
-    instance.run_callback(:before_dinner)
+    expect_evaluate_ruby do
+      instance = Foo.new
+      [ instance.respond_to?(:wash_hands), instance.run_callback(:before_dinner) ]
+    end.to eq([true, ["wash_hands"]])
   end
 
   it 'defines multiple callbacks' do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Callbacks
-      define_callback :before_dinner
+    on_client do
+      class Foo
+        include React::Callbacks
+        define_callback :before_dinner
+        before_dinner :wash_hands, :turn_off_laptop
 
-      before_dinner :wash_hand, :turn_off_laptop
-
-      def wash_hand;end
-
-      def turn_off_laptop;end
+        def wash_hands;end
+        def turn_off_laptop;end
+      end
     end
-
-    instance = Foo.new
-    expect(instance).to receive(:wash_hand)
-    expect(instance).to receive(:turn_off_laptop)
-    instance.run_callback(:before_dinner)
+    expect_evaluate_ruby do
+      instance = Foo.new
+      [ instance.respond_to?(:wash_hands),
+        instance.respond_to?(:turn_off_laptop),
+        instance.run_callback(:before_dinner) ]
+    end.to eq([true, true, ["wash_hands", "turn_off_laptop" ]])
   end
 
   context 'using Hyperloop::Context.reset!' do
-    after(:all) do
-      Hyperloop::Context.instance_variable_set(:@context, nil)
-    end
+    #after(:all) do
+    #  Hyperloop::Context.instance_variable_set(:@context, nil)
+    #end
     it 'clears callbacks on Hyperloop::Context.reset!' do
-      Hyperloop::Context.reset!
-      stub_const 'Foo', Class.new
-      Foo.class_eval do
-        include React::Callbacks
-        define_callback :before_dinner
+      on_client do
+        Hyperloop::Context.reset!
 
-        before_dinner :wash_hand, :turn_off_laptop
+        class Foo
+          include React::Callbacks
+          define_callback :before_dinner
 
-        def wash_hands;end
+          before_dinner :wash_hands, :turn_off_laptop
 
-        def turn_off_laptop;end
+          def wash_hands;end
+
+          def turn_off_laptop;end
+        end
       end
-      instance = Foo.new
-      expect(instance).to receive(:wash_hand).once
-      expect(instance).not_to receive(:turn_off_laptop)
+      expect_evaluate_ruby do
+        instance = Foo.new
 
-      Hyperloop::Context.reset!
+        Hyperloop::Context.reset!
+        
+        Foo.class_eval do
+          before_dinner :wash_hands
+        end
 
-      instance.run_callback(:before_dinner)
-      Foo.class_eval do
-        before_dinner :wash_hand
-      end
-      instance.run_callback(:before_dinner)
+        instance.run_callback(:before_dinner)
+      end.to eq(["wash_hands"])
     end
-  end # moved elswhere cause its just hard to get anything to work in this environment
+  end
 
   it 'defines block callback' do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Callbacks
-      attr_accessor :a
-      attr_accessor :b
+    on_client do
+      class Foo
+        include React::Callbacks
+        attr_accessor :a
+        attr_accessor :b
 
-      define_callback :before_dinner
+        define_callback :before_dinner
 
-      before_dinner do
-        self.a = 10
-      end
-      before_dinner do
-        self.b = 20
+        before_dinner do
+          self.a = 10
+        end
+        before_dinner do
+          self.b = 20
+        end
       end
     end
-
-    foo = Foo.new
-    foo.run_callback(:before_dinner)
-    expect(foo.a).to eq(10)
-    expect(foo.b).to eq(20)
+    expect_evaluate_ruby do
+      foo = Foo.new
+      foo.run_callback(:before_dinner)
+      [ foo.a, foo.b ]
+    end.to eq([10, 20])
   end
 
   it 'defines multiple callback group' do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Callbacks
-      define_callback :before_dinner
-      define_callback :after_dinner
-      attr_accessor :a
+    on_client do
+      class Foo
+        include React::Callbacks
+        define_callback :before_dinner
+        define_callback :after_dinner
+        attr_accessor :a
 
-      before_dinner do
-        self.a = 10
+        before_dinner do
+          self.a = 10
+        end
       end
     end
-
-    foo = Foo.new
-    foo.run_callback(:before_dinner)
-    foo.run_callback(:after_dinner)
-
-    expect(foo.a).to eq(10)
+    expect_evaluate_ruby do
+      foo = Foo.new
+      foo.run_callback(:before_dinner)
+      foo.run_callback(:after_dinner)
+      foo.a
+    end.to eq(10)
   end
 
   it 'receives args as callback' do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Callbacks
-      define_callback :before_dinner
-      define_callback :after_dinner
+    on_client do
+      class Foo
+        include React::Callbacks
+        define_callback :before_dinner
+        define_callback :after_dinner
 
-      attr_accessor :lorem
+        attr_accessor :lorem
 
-      before_dinner do |a, b|
-        self.lorem  = "#{a}-#{b}"
+        before_dinner do |a, b|
+          self.lorem  = "#{a}-#{b}"
+        end
+
+        after_dinner :eat_ice_cream
+        def eat_ice_cream(a,b,c);  end
       end
-
-      after_dinner :eat_ice_cream
-      def eat_ice_cream(a,b,c);  end
     end
-
-    foo = Foo.new
-    expect(foo).to receive(:eat_ice_cream).with(4,5,6)
-    foo.run_callback(:before_dinner, 1, 2)
-    foo.run_callback(:after_dinner, 4, 5, 6)
-    expect(foo.lorem).to eq('1-2')
+    expect_evaluate_ruby do
+      foo = Foo.new
+      foo.run_callback(:before_dinner, 1, 2)
+      res1 = foo.run_callback(:after_dinner, 4, 5, 6)
+      [res1, foo.lorem]
+    end.to eq([["eat_ice_cream"], '1-2'])
   end
-end
 end
