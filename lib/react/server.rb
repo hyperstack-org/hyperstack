902dc4c49fef165d41b8381d76198f0c03e3c32d
Jan Biedermann <jan@kursator.de>
2018-01-12 18:47:11 +0100

    remove old react things

--- lib/react/server.rb
+++ lib/react/server.rb
@@ -3,8 +3,6 @@ module React
     def self.render_to_string(element)
       if !(`typeof ReactDOMServer === 'undefined'`)
         React::RenderingContext.build { `ReactDOMServer.renderToString(#{element.to_n})` } # v0.15+
-      elsif !(`typeof React.renderToString === 'undefined'`)
-        React::RenderingContext.build { `React.renderToString(#{element.to_n})` }
       else
         raise "renderToString is not defined.  In React >= v15 you must import it with ReactDOMServer"
       end
@@ -13,8 +11,6 @@ module React
     def self.render_to_static_markup(element)
       if !(`typeof ReactDOMServer === 'undefined'`)
         React::RenderingContext.build { `ReactDOMServer.renderToStaticMarkup(#{element.to_n})` } # v0.15+
-      elsif !(`typeof React.renderToString === 'undefined'`)
-        React::RenderingContext.build { `React.renderToStaticMarkup(#{element.to_n})` }
       else
         raise "renderToStaticMarkup is not defined.  In React >= v15 you must import it with ReactDOMServer"
       end
