14bec65b10ca4f3be6c11b44ba97a332068a909b
Jan Biedermann <jan@kursator.de>
2018-01-12 18:48:41 +0100

    convert to hyper-spec

--- spec/react/dsl_spec.rb
+++ spec/react/dsl_spec.rb
@@ -1,303 +1,306 @@
 require 'spec_helper'
 
-if opal?
-describe 'the React DSL', type: :component do
+describe 'the React DSL', js: true do
 
   context "render macro" do
 
     it "can define the render method with the render macro with a html tag container" do
-      stub_const 'Foo', Class.new
-      Foo.class_eval do
-        include React::Component
-        render(:div, class: :foo) do
-          "hello"
+      mount 'Foo' do
+        class Foo
+          include React::Component
+          render(:div, class: :foo) do
+            "hello"
+          end
         end
       end
-
-      expect(Foo).to render_static_html('<div class="foo">hello</div>')
+      expect(page.body[-60..-19]).to include('<div class="foo">hello</div>')
     end
 
     it "can define the render method with the render macro without a container" do
-      stub_const 'Foo', Class.new
-      Foo.class_eval do
-        include React::Component
-        render do
-          "hello"
+      mount 'Foo' do
+        class Foo
+          include React::Component
+          render do
+            "hello"
+          end
         end
       end
-
-      expect(Foo).to render_static_html('<span>hello</span>')
+      expect(page.body[-60..-19]).to include('<span>hello</span>')
     end
 
     it "can define the render method with the render macro with a application defined container" do
-      stub_const 'Bar', Class.new(React::Component::Base)
-      Bar.class_eval do
-        param :p1
-        render { "hello #{params.p1}" }
+      on_client do
+        class Bar < React::Component::Base
+          param :p1
+          render { "hello #{params.p1}" }
+        end
+        class Foo < React::Component::Base
+          render Bar, p1: "fred"
+        end
       end
-      stub_const 'Foo', Class.new(React::Component::Base)
-      Foo.class_eval do
-        render Bar, p1: "fred"
-      end
-
-      expect(Foo).to render_static_html('<span>hello fred</span>')
+      mount 'Foo'
+      expect(page.body[-60..-19]).to include('<span>hello fred</span>')
     end
   end
 
   it "will turn the last string in a block into a element" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        div { "hello" }
-      end
-    end
-
-    expect(Foo).to render_static_html('<div>hello</div>')
-  end
-
-  it "will pass converted props through event handlers" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        INPUT(data: {foo: 12}).on(:change) {}
-      end
-    end
-
-    expect(React::Server.render_to_static_markup(React.create_element(Foo))).to match(/<input data-foo="12"(\/)?>/)
-  end
-
-  it "will turn the last string in a block into a element" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        DIV { "hello" }
-      end
-    end
-
-    expect(Foo).to render_static_html('<div>hello</div>')
-  end
-
-  it "has a .span short hand String method" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        div { "hello".span; "goodby".span }
-      end
-    end
-
-    expect(Foo).to render_static_html('<div><span>hello</span><span>goodby</span></div>')
-  end
-
-  it "has a .br short hand String method" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        div { "hello".br }
-      end
-    end
-
-    expect(React::Server.render_to_static_markup(React.create_element(Foo)).gsub("<br/>", "<br>")).to eq('<div><span>hello<br></span></div>')
-  end
-
-  it "has a .td short hand String method" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        table {
-          tbody {
-            tr { "hello".td }
-          }
-        }
-      end
-    end
-
-    expect(Foo).to render_static_html('<table><tbody><tr><td>hello</td></tr></tbody></table>')
-  end
-
-  it "has a .para short hand String method" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        div { "hello".para }
-      end
-    end
-
-    expect(Foo).to render_static_html('<div><p>hello</p></div>')
-  end
-
-  it 'can do a method call on a class name that is not a direct sibling' do
-    stub_const 'Mod', Module.new
-    stub_const 'Mod::NestedMod', Module.new
-    stub_const 'Mod::Comp', Class.new(React::Component::Base)
-    Mod::Comp.class_eval do
-      render { 'Mod::Comp' }
-    end
-    stub_const 'Mod::NestedMod::NestedComp', Class.new(React::Component::Base)
-    Mod::NestedMod::NestedComp.class_eval do
-      render do
-        Comp()
-      end
-    end
-    expect(Mod::NestedMod::NestedComp)
-      .to render_static_html('<span>Mod::Comp</span>')
-  end
-
-  it 'raises a meaningful error if a Constant Name is not actually a component' do
-    stub_const 'Mod', Module.new
-    stub_const 'Mod::NestedMod', Module.new
-    stub_const 'Mod::Comp', Class.new
-    stub_const 'Mod::NestedMod::NestedComp', Class.new(React::Component::Base)
-    Mod::NestedMod::NestedComp.class_eval do
-      backtrace :none
-      render do
-        Comp()
-      end
-    end
-    expect { React::Server.render_to_static_markup(React.create_element(Mod::NestedMod::NestedComp)) }
-      .to raise_error('Comp does not appear to be a react component.')
-  end
-
-  it 'raises a method missing error' do
-    stub_const 'Foo', Class.new(React::Component::Base)
-    Foo.class_eval do
-      backtrace :none
-      render do
-        _undefined_method
-      end
-    end
-    expect { React::Server.render_to_static_markup(React.create_element(Foo)) }
-      .to raise_error(NoMethodError)
-  end
-
-  it "will treat the component class name as a first class component name" do
-    stub_const 'Mod::Bar', Class.new
-    Mod::Bar.class_eval do
-      include React::Component
-      def render
-        "a man walks into a bar"
-      end
-    end
-    stub_const 'Foo', Class.new(React::Component::Base)
-    Foo.class_eval do
-      def render
-        Mod::Bar()
-      end
-    end
-
-    expect(Foo).to render_static_html('<span>a man walks into a bar</span>')
-  end
-
-  it "can add class names by the haml .class notation" do
-    stub_const 'Mod::Bar', Class.new
-    Mod::Bar.class_eval do
-      include React::Component
-      collect_other_params_as :attributes
-      def render
-        "a man walks into a bar".span(params.attributes)
-      end
-    end
-    stub_const 'Foo', Class.new(React::Component::Base)
-    Foo.class_eval do
-      def render
-        Mod::Bar().the_class.other_class
-      end
-    end
-
-    expect(Foo).to render_static_html('<span class="other-class the-class">a man walks into a bar</span>')
-  end
-
-  it "can use the 'class' keyword for classes" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        span(class: "the-class") { "hello" }
-      end
-    end
-
-    expect(Foo).to render_static_html('<span class="the-class">hello</span>')
-  end
-
-  it "can generate a unrendered node using the .as_node method" do          # div { "hello" }.as_node
-    stub_const 'Foo', Class.new #(React::Component::Base)
-    Foo.class_eval do
-      include React::Component
-      def render
-        span(data: {size: 12}) { "hello".span.as_node.class.name }.as_node.render
-      end
-    end
-
-    expect(Foo)
-    .to render_static_html('<span data-size="12">React::Element</span>')
-  end
-
-  it "can use the dangerously_set_inner_HTML param" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        div(dangerously_set_inner_HTML:  { __html: "Hello&nbsp;&nbsp;Goodby" })
-      end
-    end
-
-    expect(Foo).to render_static_html('<div>Hello&nbsp;&nbsp;Goodby</div>')
-  end
-
-  it 'should convert a hash param to hyphenated html attributes if in React::HASH_ATTRIBUTES' do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        div(data: { foo: :bar }, aria: { label: "title" })
-      end
-    end
-
-    expect(Foo)
-      .to render_static_html('<div data-foo="bar" aria-label="title"></div>')
-  end
-
-  it 'should not convert a hash param to hyphenated html attributes if not in React::HASH_ATTRIBUTES' do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      def render
-        div(title: { bar: :foo })
-      end
-    end
-
-    expect(Foo)
-      .to render_static_html(
-        '<div title="{&quot;bar&quot;=&gt;&quot;foo&quot;}"></div>'
-      )
-  end
-
-  it "will remove all elements passed as params from the rendering buffer" do
-    stub_const 'X2', Class.new
-    X2.class_eval do
-      include React::Component
-      def render
-        div do
-          params[:ele].render
-          params[:ele].render
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          div { "hello" }
         end
       end
     end
-    stub_const 'Test', Class.new
-    Test.class_eval do
-      include React::Component
-      def render
-        X2(ele: b { "hello" })
+    expect(page.body[-60..-19]).to include('<div>hello</div>')
+  end
+
+  it "in prerender will pass converted props through event handlers" do
+    client_option render_on: :both
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          INPUT(data: {foo: 12}).on(:change) {}
+        end
       end
     end
-
-    expect(Test).to render_static_html('<div><b>hello</b><b>hello</b></div>')
+    expect(page.body).to match(/<input data-foo="12" data-reactroot="" \/>/)
   end
-end
+
+  it "will turn the last string in a block into a element" do
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          DIV { "hello" }
+        end
+      end
+    end
+    expect(page.body[-60..-19]).to include('<div>hello</div>')
+  end
+
+  it "has a .span short hand String method" do
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          div { "hello".span; "goodby".span }
+        end
+      end
+    end
+    expect(page.body[-70..-19]).to include('<div><span>hello</span><span>goodby</span></div>')
+  end
+
+  it "in prerendering has a .br short hand String method" do
+    client_option render_on: :both
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          div { "hello".br }
+        end
+      end
+    end
+    expect(page.body[-80..-19].gsub("<br/>", "<br>").gsub("<br />", "<br>")).to include('<div data-reactroot=""><span>hello<br></span></div>')
+  end
+
+  it "has a .td short hand String method" do
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          table {
+            tbody {
+              tr { "hello".td }
+            }
+          }
+        end
+      end
+    end
+    expect(page.body[-90..-19]).to include('<table><tbody><tr><td>hello</td></tr></tbody></table>')
+  end
+
+  it "has a .para short hand String method" do
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          div { "hello".para }
+        end
+      end
+    end
+    expect(page.body[-60..-19]).to include('<div><p>hello</p></div>')
+  end
+
+  it 'can do a method call on a class name that is not a direct sibling' do
+    mount 'Mod::NestedMod::NestedComp' do
+      module Mod
+        class Comp < React::Component::Base
+          render { "Mod::Comp" }
+        end
+        module NestedMod
+          class NestedComp < React::Component::Base
+            render do
+              Comp()
+            end
+          end
+        end
+      end
+    end
+    expect(page.body[-60..-19]).to include('<span>Mod::Comp</span>')
+  end
+
+  it 'raises a meaningful error if a Constant Name is not actually a component' do
+    mount 'Mod::NestedMod::NestedComp' do
+      module Mod
+        module NestedMod
+          class NestedComp < React::Component::Base
+            backtrace :none
+            render do
+              Comp()
+            end
+          end
+        end
+        class Comp; end
+      end
+    end
+    expect(page.driver.browser.manage.logs.get(:browser).map { |m| m.message.gsub(/\\n/, "\n") }.to_a.join("\n"))
+          .to match(/Comp does not appear to be a react component./)
+  end
+
+  it 'raises a method missing error' do
+    client_option render_on: :both
+    mount 'Foo' do
+      class Foo < React::Component::Base
+        backtrace :none
+        render do
+          _undefined_method
+        end
+      end
+    end
+    expect(page.driver.browser.manage.logs.get(:browser).map { |m| m.message.gsub(/\\n/, "\n") }.to_a.join("\n"))
+          .to match(/NoMethodError/)
+      
+  end
+
+  it "will treat the component class name as a first class component name" do
+    mount 'Foo' do
+      module Mod
+        class Bar
+          include React::Component
+          def render
+            "a man walks into a bar"
+          end
+        end
+      end
+      class Foo < React::Component::Base
+        def render
+          Mod::Bar()
+        end
+      end
+    end
+    expect(page.body[-60..-19]).to include('<span>a man walks into a bar</span>')
+  end
+
+  it "can add class names by the haml .class notation" do
+    mount 'Foo' do
+      module Mod
+        class Bar
+          include React::Component
+          collect_other_params_as :attributes
+          def render
+            "a man walks into a bar".span(params.attributes)
+          end
+        end
+      end
+      class Foo < React::Component::Base
+        def render
+          Mod::Bar().the_class.other_class
+        end
+      end
+    end
+    expect(page.body[-90..-19]).to include('<span class="other-class the-class">a man walks into a bar</span>')
+  end
+
+  it "can use the 'class' keyword for classes" do
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          span(class: "the-class") { "hello" }
+        end
+      end
+    end
+    expect(page.body[-60..-19]).to include('<span class="the-class">hello</span>')
+  end
+
+  it "can generate a unrendered node using the .as_node method" do          # div { "hello" }.as_node
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          span(data: {size: 12}) { "hello".span.as_node.class.name }.as_node.render
+        end
+      end
+    end
+    expect(page.body[-80..-19]).to include('<span data-size="12">React::Element</span>')
+  end
+
+  it "can use the dangerously_set_inner_HTML param" do
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          div(dangerously_set_inner_HTML:  { __html: "Hello and Goodby" })
+        end
+      end
+    end
+    expect(page.body[-60..-19]).to include('<div>Hello and Goodby</div>')
+  end
+
+  it 'should convert a hash param to hyphenated html attributes if in React::HASH_ATTRIBUTES' do
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          div(data: { foo: :bar }, aria: { label: "title" })
+        end
+      end
+    end
+    expect(page.body[-70..-19]).to include('<div data-foo="bar" aria-label="title"></div>')
+  end
+
+  it 'should not convert a hash param to hyphenated html attributes if not in React::HASH_ATTRIBUTES' do
+    mount 'Foo' do
+      class Foo
+        include React::Component
+        def render
+          div(title: { bar: :foo })
+        end
+      end
+    end
+    expect(page.body[-80..-19]).to include('<div title="{&quot;bar&quot;=&gt;&quot;foo&quot;}"></div>')
+  end
+
+  it "will remove all elements passed as params from the rendering buffer" do
+    mount 'Foo' do
+      class X2
+        include React::Component
+        def render
+          div do
+            params[:ele].render
+            params[:ele].render
+          end
+        end
+      end
+      class Foo
+        include React::Component
+        def render
+          X2(ele: b { "hello" })
+        end
+      end
+    end
+    expect(page.body[-60..-19]).to include('<div><b>hello</b><b>hello</b></div>')
+  end
 end
