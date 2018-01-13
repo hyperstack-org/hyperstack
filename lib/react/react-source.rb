b6bc4c0f801fe9175bd62d30e5d7c87b8b3fce23
Jan Biedermann <jan@kursator.de>
2018-01-13 20:52:03 +0100

    use default config, react addon are no longer availabe in react16

--- lib/react/react-source.rb
+++ lib/react/react-source.rb
@@ -11,10 +11,6 @@ if RUBY_ENGINE == 'opal'
 else
   require "react/config"
   require "react/rails/asset_variant"
-  react_directory = React::Rails::AssetVariant.new(
-    addons: true,
-    variant: React::Config.config[:environment].to_sym
-  ).react_directory
+  react_directory = React::Rails::AssetVariant.new(React::Config.config).react_directory
   Opal.append_path react_directory.untaint
-  Opal.append_path File.expand_path('../../react-sources/', __FILE__).untaint
 end
