e96fa76e472ee20e6f0c00b93ecf13730940c109
Jan Biedermann <jan@kursator.de>
2018-01-13 20:53:00 +0100

    remove precompiled assets for tests

--- spec/reactive-ruby/rails/asset_pipeline_spec.rb
+++ spec/reactive-ruby/rails/asset_pipeline_spec.rb
@@ -2,7 +2,8 @@ require 'spec_helper'
 
 RSpec.describe 'test_app generator' do
   it "does not interfer with asset precompilation" do
-    cmd = "cd spec/test_app; BUNDLE_GEMFILE=#{ENV['REAL_BUNDLE_GEMFILE']} bundle exec rake assets:precompile"
+    cmd = "cd spec/test_app; BUNDLE_GEMFILE=#{ENV['REAL_BUNDLE_GEMFILE']} bundle exec rails assets:precompile"
     expect(system(cmd)).to be_truthy
+    "cd spec/test_app; BUNDLE_GEMFILE=#{ENV['REAL_BUNDLE_GEMFILE']} bundle exec rails assets:clobber"
   end
 end
