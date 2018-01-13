1b5b649eaa1e73fac5ae4ab05ad9314760476600
Jan Biedermann <jan@kursator.de>
2018-01-13 23:12:27 +0100

    run them all :)

--- spec/react/event_spec.rb
+++ spec/react/event_spec.rb
@@ -2,21 +2,22 @@ require "spec_helper"
 
 describe 'React::Event', js: true do
   it "should bridge attributes of native SyntheticEvent (see http://facebook.github.io/react/docs/events.html#syntheticevent)" do
+    client_option deprecation_warnings: :off
     expect_evaluate_ruby do
       results = {}
       element = React.create_element('div').on(:click) do |event|
         results[:bubbles] = event.bubbles == event.to_n.JS[:bubbles]
         results[:cancelable] = event.cancelable == event.to_n.JS[:cancelable]
-        results[:current_target] = event.current_target == event.JS[:currentTarget]
-        results[:default_prevented] = event.default_prevented == event.JS[:defaultPrevented]
-        results[:event_phase] = event.event_phase == event.JS[:eventPhase]
-        results[:is_trusted?] = event.is_trusted? == event.JS[:isTrusted]
-        results[:native_event] = event.native_event == event.JS[:nativeEvent]
-        results[:target] = event.target == event.JS[:target]
-        results[:timestamp] = event.timestamp == event.JS[:timeStamp]
-        results[:event_type] = event_event_type == event.JS[:type]
-        results[:prevent_default] = event.respond_to(:prevent_default)
-        results[:stop_propagation] = event.respond_to(:stop_propagation)
+        results[:current_target] = event.current_target == event.to_n.JS[:currentTarget]
+        results[:default_prevented] = event.default_prevented == event.to_n.JS[:defaultPrevented]
+        results[:event_phase] = event.event_phase == event.to_n.JS[:eventPhase]
+        results[:is_trusted?] = event.is_trusted? == event.to_n.JS[:isTrusted]
+        results[:native_event] = event.native_event == event.to_n.JS[:nativeEvent]
+        results[:target] = event.target == event.to_n.JS[:target]
+        results[:timestamp] = event.timestamp == event.to_n.JS[:timeStamp]
+        results[:event_type] = event.event_type == event.to_n.JS[:type]
+        results[:prevent_default] = event.respond_to?(:prevent_default)
+        results[:stop_propagation] = event.respond_to?(:stop_propagation)
       end
       dom_node = React::Test::Utils.render_into_document(element)
       React::Test::Utils.simulate_click(dom_node)
