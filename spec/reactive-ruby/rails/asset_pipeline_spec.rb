84ebcfcaddece485b8b91d71131d6c4e1ed44908
Jan Biedermann <jan@kursator.de>
2018-01-13 20:53:26 +0100

    donr use precompiled assets for tests

--- spec/reactive-ruby/rails/asset_pipeline_spec.rb
+++ spec/reactive-ruby/rails/asset_pipeline_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 RSpec.describe 'test_app generator' do
-  it "does not interfer with asset precompilation" do
+  it "does not interfere with asset precompilation" do
     cmd = "cd spec/test_app; BUNDLE_GEMFILE=#{ENV['REAL_BUNDLE_GEMFILE']} bundle exec rails assets:precompile"
     expect(system(cmd)).to be_truthy
     "cd spec/test_app; BUNDLE_GEMFILE=#{ENV['REAL_BUNDLE_GEMFILE']} bundle exec rails assets:clobber"
