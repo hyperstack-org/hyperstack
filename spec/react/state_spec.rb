e78628d16416c13d364d1bd7c73086a7c9b82b48
Jan Biedermann <jan@kursator.de>
2018-01-13 21:29:27 +0100

    convert to hyper-spec

--- spec/react/state_spec.rb
+++ spec/react/state_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
-if opal?
-describe 'React::State' do
+describe 'React::State', js: true do
   it "can create dynamically initialized exported states" do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
-      export_state(:foo) { 'bar' }
-    end
-    Hyperloop::Application::Boot.run
-    expect(Foo.foo).to eq('bar')
+    expect_evaluate_ruby do
+      class Foo
+        include React::Component
+        export_state(:foo) { 'bar' }
+      end
+      Hyperloop::Application::Boot.run
+      Foo.foo
+    end.to eq('bar')
   end
 
   # these will all require async operations and testing to see if things get
@@ -24,32 +24,29 @@ describe 'React::State' do
   it "can be accessed outside of react using get/set_state"
 
   it 'ignores state updates during rendering' do
-    stub_const 'StateTest', Class.new(React::Component::Base)
-    StateTest.class_eval do
-      export_state :boom
-      before_mount do
-        # force boom to be on the observing list during the current rendering cycle
-        StateTest.boom! !StateTest.boom
-        # this is automatically called by after_mount / after_update, but we don't want
-        # to have to setup a complicated async test, so we just force it now.
-        # if we don't do this, then updating boom will have no effect on the first render
-        React::State.update_states_to_observe
+    client_option render_on: :both
+    evaluate_ruby do
+      class StateTest < React::Component::Base
+        export_state :boom
+        before_mount do
+          # force boom to be on the observing list during the current rendering cycle
+          StateTest.boom! !StateTest.boom
+          # this is automatically called by after_mount / after_update, but we don't want
+          # to have to setup a complicated async test, so we just force it now.
+          # if we don't do this, then updating boom will have no effect on the first render
+          React::State.update_states_to_observe
+        end
+        def render
+          (StateTest.boom ? "Boom" : "No Boom").tap { StateTest.boom! !StateTest.boom }
+        end
       end
-      def render
-        (StateTest.boom ? "Boom" : "No Boom").tap { StateTest.boom! !StateTest.boom }
-      end
+      MARKUP = React::Server.render_to_static_markup(React.create_element(StateTest))
     end
-    %x{
-      var log = [];
-      var org_warn_console =  window.console.warn;
-      var org_error_console = window.console.error;
-      window.console.warn = window.console.error = function(str){log.push(str)}
-    }
-    markup = React::Server.render_to_static_markup(React.create_element(StateTest))
-    `window.console.warn = org_warn_console; window.console.error = org_error_console;`
-    expect(markup).to eq('<span>Boom</span>')
-    expect(StateTest.boom).to be_falsy
-    expect(`log`).to eq([])
+    expect_evaluate_ruby("MARKUP").to eq('<span>Boom</span>')
+    expect_evaluate_ruby("StateTest.boom").to be_falsy
+    expect(page.driver.browser.manage.logs.get(:browser).reject { |entry|
+      entry_s = entry.to_s
+      entry_s.include?("Deprecated feature") || entry_s.include?("Mount() on the server. This is a no-op.")
+    }.size).to eq(0)
   end
-end
 end
