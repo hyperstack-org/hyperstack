5ed1f75e8bb64ef79c8429d137bb5463509c7ec1
Jan Biedermann <jan@kursator.de>
2018-01-14 16:15:55 +0100

    fix

--- spec/react/refs_callback_spec.rb
+++ spec/react/refs_callback_spec.rb
@@ -1,56 +1,58 @@
 require 'spec_helper'
 
-if opal?
-
-describe 'Refs callback' do
+describe 'Refs callback', js: true do
   before do
-    stub_const 'Foo', Class.new
-    Foo.class_eval do
-      include React::Component
+    on_client do
+      class Foo
+        include React::Component
+        def self.bar
+          @@bar ||= 'club'
+        end
+      end
     end
   end
 
   it "is invoked with the actual Ruby instance" do
-    stub_const 'Bar', Class.new
-    Bar.class_eval do
-      include React::Component
-      def render
-        React.create_element('div')
-      end
-    end
-
-    Foo.class_eval do
-      def my_bar=(bar)
-        $bar = bar
+    expect_evaluate_ruby do
+      class Bar
+        include React::Component
+        def render
+          React.create_element('div')
+        end
       end
 
-      def render
-        React.create_element(Bar, ref: method(:my_bar=).to_proc)
-      end
-    end
+      Foo.class_eval do
+        def my_bar=(bars)
+          @@bar = bars
+        end
 
-    element = React.create_element(Foo)
-    React::Test::Utils.render_into_document(element)
-    expect($bar).to be_a(Bar)
-    $bar = nil
+        def render
+          React.create_element(Bar, ref: method(:my_bar=).to_proc)
+        end
+      end
+
+      element = React.create_element(Foo)
+      React::Test::Utils.render_into_document(element)
+      Foo.bar.class.name
+    end.to eq('Bar')
   end
 
   it "is invoked with the actual DOM node" do
-    Foo.class_eval do
-      def my_div=(div)
-        $div = div
+    # client_option raise_on_js_errors: :off
+    expect_evaluate_ruby do
+      Foo.class_eval do
+        def my_div=(div)
+          @@bar = div
+        end
+
+        def render
+          React.create_element('div', ref: method(:my_div=).to_proc)
+        end
       end
 
-      def render
-        React.create_element('div', ref: method(:my_div=).to_proc)
-      end
-    end
-
-    element = React.create_element(Foo)
-    React::Test::Utils.render_into_document(element)
-    expect(`#{$div}.nodeType`).to eq(1)
-    $div = nil
+      element = React.create_element(Foo)
+      React::Test::Utils.render_into_document(element)
+      Foo.bar.JS['nodeType']
+    end.to eq(1)
   end
-end
-
 end
