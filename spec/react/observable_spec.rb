77daa33453f0a1577a8020cafb02572861c9cf28
Jan Biedermann <jan@kursator.de>
2018-01-14 16:15:32 +0100

    fix

--- spec/react/observable_spec.rb
+++ spec/react/observable_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'React::Observable', js: true do
   it "allows to set value on Observable" do
-    expect_evaluate_ruby do
+    mount 'Foo' do
       class Zoo
         include React::Component
         param :foo, type: React::Observable
@@ -25,9 +25,7 @@ describe 'React::Observable', js: true do
           end
         end
       end
-
-      instance = React::Test::Utils.render_component_into_document(Foo)
-      instance.dom_node.JS[:innerHTML]
-    end.to eq('<span></span><span>4</span>')
+    end
+    expect(page.body[-60..-19]).to include('<span></span><span>4</span>')
   end
 end
