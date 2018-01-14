f7e986d7efdba4ebbf185be9ad9deb7d414c04f2
Jan Biedermann <jan@kursator.de>
2018-01-14 22:20:45 +0100

    add test_app to repo, because of to many configuration changes

---
+++ spec/test_app/app/assets/javascripts/application.rb
@@ -0,0 +1,7 @@
+require 'jquery'
+require 'react.js'
+require 'react-server.js'
+require 'react_ujs'
+require 'opal'
+require 'opal-jquery'
+require 'components'
---
+++ spec/test_app/app/views/components.rb
@@ -0,0 +1,12 @@
+require 'hyper-react'
+if React::IsomorphicHelpers.on_opal_client?
+  require 'browser'
+  require 'browser/delay'
+end
+require 'react/ref_callback'
+require 'react/server'
+require 'react/test/utils'
+require 'reactrb/auto-import'
+require 'js'
+
+require_tree './components'
