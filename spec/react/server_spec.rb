6c10fc5897cd9352bae11ad69a627e0dff7a0566
Jan Biedermann <jan@kursator.de>
2018-01-14 03:40:31 +0100

    convert to hyper-spec

--- spec/react/server_spec.rb
+++ spec/react/server_spec.rb
@@ -1,25 +1,25 @@
 require "spec_helper"
 
-if opal?
-
-RSpec.describe React::Server do
-  after(:each) do
-    React::API.clear_component_class_cache
-  end
+describe 'React::Server', js: true do
 
   describe "render_to_string" do
     it "should render a React.Element to string" do
-      ele = React.create_element('span') { "lorem" }
-      expect(React::Server.render_to_string(ele)).to be_kind_of(String)
+      client_option render_on: :both
+      expect_evaluate_ruby do
+        ele = React.create_element('span') { "lorem" }
+        React::Server.render_to_string(ele).class.name
+      end.to eq("String")
     end
   end
 
   describe "render_to_static_markup" do
     it "should render a React.Element to static markup" do
-      ele = React.create_element('span') { "lorem" }
-      expect(React::Server.render_to_static_markup(ele)).to eq("<span>lorem</span>")
+      client_option render_on: :both
+      expect_evaluate_ruby do
+        ele = React.create_element('span') { "lorem" }
+        React::Server.render_to_static_markup(ele)
+      end.to eq("<span>lorem</span>")
     end
   end
 end
 
-end
