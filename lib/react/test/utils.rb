16c4b92a1de72e5b9d5ae98e506370ac4b0f56b4
Jan Biedermann <jan@kursator.de>
2018-01-14 02:31:32 +0100

    fixes and improvements

--- lib/react/test/utils.rb
+++ lib/react/test/utils.rb
@@ -8,23 +8,61 @@ module React
 
       def self.render_into_document(element)
         raise "You should pass a valid React::Element" unless React.is_valid_element?(element)
-        dom_el = JS.call(:eval, "document.body.appendChild(document.createElement('div'))")
+        dom_el = `document.body.appendChild(document.createElement('div'))`
         React.render(element, dom_el)
       end
 
       def self.simulate_click(element)
-        # element must be a component or a dom node
-        el = if element.is_a? React::Component
-               element.dom_node
-             else
-               element
-             end
+        # element must be a component or a dom node or a element
+        el =  if `typeof element.nodeType !== "undefined"`
+                element
+              elsif element.is_a? React::Component
+                element.dom_node
+              elsif element.is_a? React::Element
+                `ReactDOM.findDOMNode(#{element.to_n}.native)`
+              else
+                element
+              end
         %x{
           var evob = new MouseEvent('click', {
             view: window,
             bubbles: true,
             cancelable: true
           });
+          el.dispatchEvent(evob);
+        }
+      end
+
+      def self.simulate_keydown(element, key_name = "Enter")
+        # element must be a component or a dom node or a element
+        el =  if `typeof element.nodeType !== "undefined"`
+                element
+              elsif element.is_a? React::Component
+                element.dom_node
+              elsif element.is_a? React::Element
+                `ReactDOM.findDOMNode(#{element.to_n}.native)`
+              else
+                element
+              end
+        %x{
+          var evob = new KeyboardEvent('keydown', { key: key_name, bubbles: true, cancelable: true });
+          el.dispatchEvent(evob);
+        }
+      end
+
+      def self.simulate_submit(element)
+        # element must be a component or a dom node or a element
+        el =  if `typeof element.nodeType !== "undefined"`
+                element
+              elsif element.is_a? React::Component
+                element.dom_node
+              elsif element.is_a? React::Element
+                `ReactDOM.findDOMNode(#{element.to_n}.native)`
+              else
+                element
+              end
+        %x{
+          var evob = new Event('submit', { bubbles: true, cancelable: true });
           el.dispatchEvent(evob);
         }
       end
