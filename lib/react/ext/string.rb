bc629380a63a414a9a50e18c8ab834e6a2552c51
Jan Biedermann <jan@kursator.de>
2018-01-17 21:34:35 +0100

    fix for opal 0.11

--- lib/react/ext/string.rb
+++ lib/react/ext/string.rb
@@ -1,6 +1,6 @@
 class String
   def event_camelize
-    `#{self}.replace(/(^|_)([^_]+)/g, function(match, pre, word, index) {
+    `return #{self}.replace(/(^|_)([^_]+)/g, function(match, pre, word, index) {
       var capitalize = true;
       return capitalize ? word.substr(0,1).toUpperCase()+word.substr(1) : word;
     })`
