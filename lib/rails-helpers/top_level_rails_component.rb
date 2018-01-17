eac1652c6f79b7fe58d4894ab7f3e829e1cdd5ac
Jan Biedermann <jan@kursator.de>
2018-01-17 23:35:11 +0100

    refactor

--- lib/rails-helpers/top_level_rails_component.rb
+++ lib/rails-helpers/top_level_rails_component.rb
@@ -3,7 +3,7 @@ module React
     include Hyperloop::Component::Mixin
 
     def self.search_path
-      @search_path ||= [Module]
+      @search_path ||= [Object]
     end
 
     export_component
@@ -16,36 +16,45 @@ module React
 
     def render
       paths_searched = []
-      if params.component_name.start_with? "::"
-        paths_searched << params.component_name.gsub(/^\:\:/,"")
+      component = nil
+      if params.component_name.start_with?('::')
+        # if absolute path of component is given, look it up and fail if not found
         component = begin
-                      params.component_name.gsub(/^\:\:/,"").split("::").inject(Module) { |scope, next_const| scope.const_get(next_const, false) }
-                    rescue Exception
+                      Object.const_get(params.component_name)
+                    rescue NameError
                       nil
                     end
-        return React::RenderingContext.render(component, params.render_params) if component && component.method_defined?(:render)
       else
-        self.class.search_path.each do |path|
-          # try each path + params.controller + params.component_name
-          paths_searched << "#{path.name + '::' unless path == Module}#{params.controller}::#{params.component_name}"
+        # if relative path is given, look it up like this
+        # 1) we check each path + controller-name + component-name
+        # 2) if we can't find it there we check each path + component-name
+        # if we can't find it we just try const_get
+        # so (assuming controller name is Home)
+        # ::Foo::Bar will only resolve to some component named ::Foo::Bar
+        # but Foo::Bar will check (in this order) ::Home::Foo::Bar, ::Components::Home::Foo::Bar, ::Foo::Bar, ::Components::Foo::Bar
+        self.class.search_path.each do |scope|
+          paths_searched << "#{scope.name}::#{params.controller}::#{params.component_name}"
           component = begin
-                        "#{params.controller}::#{params.component_name}".split("::").inject(path) { |scope, next_const| scope.const_get(next_const, false) }
-                      rescue Exception
+                        scope.const_get(params.controller, false).const_get(params.component_name, false)
+                      rescue NameError
                         nil
                       end
-          return React::RenderingContext.render(component, params.render_params) if component && component.method_defined?(:render)
+          break if component != nil
         end
-        self.class.search_path.each do |path|
-          # then try each path + params.component_name
-          paths_searched << "#{path.name + '::' unless path == Module}#{params.component_name}"
-          component = begin
-                        "#{params.component_name}".split("::").inject(path) { |scope, next_const| scope.const_get(next_const, false) }
-                      rescue Exception
-                        nil
-                      end
-          return React::RenderingContext.render(component, params.render_params) if component && component.method_defined?(:render)
+        unless component
+          self.class.search_path.each do |scope|
+            paths_searched << "#{scope.name}::#{params.component_name}"
+            component = begin
+                          scope.const_get(params.component_name, false)
+                        rescue NameError
+                          nil
+                        end
+            break if component != nil
+          end
         end
       end
+      puts "paths searched: #{paths_searched}"
+      return React::RenderingContext.render(component, params.render_params) if component && component.method_defined?(:render)
       raise "Could not find component class '#{params.component_name}' for params.controller '#{params.controller}' in any component directory. Tried [#{paths_searched.join(", ")}]"
     end
   end
