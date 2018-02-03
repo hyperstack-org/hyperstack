476e62d8635ad66839c496d957194b9c4ff613e5
Jan Biedermann <jan@kursator.de>
2018-02-03 20:19:27 +0100

    run firefox and chrome

--- dciy_run.sh
+++ dciy_run.sh
@@ -2,4 +2,9 @@
 export HYPER_DEV_GEM_SOURCE="https://gems.ruby-hyperloop.org"
 export RAILS_ENV="test"
 pwd
-bundle exec rspec
+echo
+echo "Running with Chrome headless"
+DRIVER=headless bundle exec rspec
+echo
+echo "Running with Firefox headless"
+DRIVER=beheaded bundle exec rspec
