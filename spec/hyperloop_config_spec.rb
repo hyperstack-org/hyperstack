dce7e3875a5b20f8c00f2facc74c78f73df3cedc
catmando <mitch@catprint.com>
2018-03-01 18:52:18 -0500

    closes #14

--- spec/hyperloop_config_spec.rb
+++ spec/hyperloop_config_spec.rb
@@ -7,11 +7,16 @@ describe 'Hyperloop Auto Config', js: true do
     Timecop.return
   end
 
-  it 'will find and load everything and expand erb files' do
+  it 'will find and load everything and expand erb files and set Hyperloop.env' do
     `rm -rf spec/test_app/tmp/cache`
     Timecop.freeze
     visit '/'
     expect(evaluate_script("Opal.Test.$const_get('TIME')")).to eq("#{Time.now}")
+    binding.pry
+    %w[test production staging development etc].each do |env|
+      expect(evaluate_script("Opal.Hyperloop.$env().$send('#{env}?')")).to eq(Rails.env.send(:"#{env}?"))
+      expect(Hyperloop.env).to eq(Rails.env)
+    end
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ require 'hyper-spec'
 require 'pry'
 require 'hyperloop-config'
 
-ENV['RAILS_ENV'] ||= 'test'
+ENV['RAILS_ENV'] = 'test'
 require File.expand_path('../test_app/config/environment', __FILE__)
 
 require 'rspec/rails'
