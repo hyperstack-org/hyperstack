bf79d4e9a047e26cbd5e9990204907a2974e84d2
Jan Biedermann <jan@kursator.de>
2018-03-26 02:08:29 +0200

    spelling and clarity

--- lib/react/component/api.rb
+++ lib/react/component/api.rb
@@ -32,15 +32,15 @@ module React
       def set_or_replace_state_or_prop(state_or_prop, method, &block)
         raise "No native ReactComponent associated" unless @native
         `var state_prop_n = #{state_or_prop.shallow_to_n}`
-        # the state object is initalized when the ruby component is instanciated
+        # the state object is initalized when the ruby component is instantiated
         # this is detected by self.native.__opalInstanceInitializedState
-        # which is set in the netive component constructor in react/api.rb
+        # which is set in the native component constructor in react/api.rb
         # the setState update callback is not called when initalizing initial state
         if block
           %x{
             if (#{@native}.__opalInstanceInitializedState === true) {
               #{@native}[method](state_prop_n, function(){
-                #{block.call}
+                block.$call();
               });
             } else {
               for (var sp in state_prop_n) {
