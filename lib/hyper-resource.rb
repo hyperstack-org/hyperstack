d4066981aafe173e989f32d5865454c3cca727d1
Jan Biedermann <jan@kursator.de>
2018-04-06 01:17:51 +0200

    fix

--- lib/hyper-resource.rb
+++ lib/hyper-resource.rb
@@ -10,7 +10,7 @@ if RUBY_ENGINE == 'opal'
   require 'hyper-store'
   require 'hyper_record'
 else
-  require 'hyper_record/client_drivers' # initialize options for the client
+  require 'hyperloop/resource/client_drivers' # initialize options for the client
   require 'hyperloop/resource/pub_sub' # server side, controller helper methods
   require 'hyperloop/resource/security_guards' # server side, controller helper methods
   Opal.append_path __dir__.untaint
