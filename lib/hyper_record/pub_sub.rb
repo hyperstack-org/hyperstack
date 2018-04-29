6ba22db57c7d3b28a4a68afce7123d65cd0baf4c
Jan Biedermann <jan@kursator.de>
2018-04-29 19:15:13 +0200

    support current_rest_method_params

--- lib/hyper_record/pub_sub.rb
+++ lib/hyper_record/pub_sub.rb
@@ -1,10 +1,14 @@
 module HyperRecord
   module PubSub
     def self.included(base)
+      attr_accessor :current_rest_method_params
+
       base.extend(HyperRecord::PubSub::ClassMethods)
     end
 
     module ClassMethods
+      attr_accessor :current_rest_method_params
+
       def _pusher_client
         Hyperloop.pusher_instance ||= Pusher::Client.new(
           app_id: Hyperloop.pusher[:app_id],
