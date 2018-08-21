7488db7f4ae350b4cddbc26a873c10cb03dcc3ef
Jan Biedermann <jan@kursator.de>
2018-08-21 11:22:35 +0200

    remove test_app

--- spec/test_app/app/assets/javascripts/application.rb
+++
@@ -1,7 +0,0 @@
-require 'jquery'
-require 'react.js'
-require 'react-server.js'
-require 'react_ujs'
-require 'opal'
-require 'opal-jquery'
-require 'components'
--- spec/test_app/config/boot.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems'
-gemfile = File.expand_path("../../../../Gemfile", __FILE__)
-
-ENV['BUNDLE_GEMFILE'] = gemfile
-require 'bundler'
-Bundler.setup
