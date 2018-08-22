a49e84de27ea7381de60ef46a9c3a1b07402a0ea
Jan Biedermann <jan@kursator.de>
2018-08-22 12:05:04 +0200

    api path belongs to transport

--- lib/hyperstack/business/config.rb
+++ lib/hyperstack/business/config.rb
@@ -3,11 +3,8 @@ if RUBY_ENGINE != 'opal'
 
     # available settings
     class << self
-      attr_accessor :api_path
       attr_accessor :valid_business_class_names
     end
-
-    self.add_client_options(%i[api_path])
 
     # default values
     self.valid_business_class_names = []
