202fe992591ca534a49de3ddab0f34791d8c6e89
Jan Biedermann <jan@kursator.de>
2018-08-23 01:36:58 +0200

    fix scope and channel(s)

--- lib/hyperstack/transport/action_cable/server_driver.rb
+++ lib/hyperstack/transport/action_cable/server_driver.rb
@@ -3,8 +3,12 @@ module Hyperstack
     module ActionCable
       class ServerDriver
         def self.publish(channels, message)
-          channels.each do |channel|
-            ActionCable.server.broadcast(channel, message)
+          if channels.class == String # just one channel
+            ::ActionCable.server.broadcast(channels, message)
+          else
+            channels.each do |channel| # many channels
+              ::ActionCable.server.broadcast(channel, message)
+            end
           end
         end
       end
