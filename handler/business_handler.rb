45fc3a1ba9af67a336f907d17ab9440f84bab98c
Jan Biedermann <jan@kursator.de>
2018-08-27 00:18:44 +0200

    use authorization

--- handler/business_handler.rb
+++ handler/business_handler.rb
@@ -1,8 +1,6 @@
 class BusinessHandler
   include Hyperstack::Business::SecurityGuards
-
-  # enable authorization if needed, depends on hyper-gate, also see below
-  # include Hyperstack::Gate
+  include Hyperstack::Gate if Hyperstack.business_use_authorization
 
   def process_request(_session_id, current_user, request)
     result = {}
@@ -12,11 +10,17 @@ class BusinessHandler
 
       business_operation = guarded_business_class(business_operation_name)
 
-      # enable authorization if needed, depends on hyper-gate, also see above
-      # authorize(current_user, business_operation, :run)
-
       request[business_operation_name].keys.each do |agent_object_id|
         parsed_params = Oj.load(request[business_operation_name][agent_object_id], symbol_keys: true)
+
+        if Hyperstack.business_use_authorization
+          authorization_result = authorize(current_user, business_operation, :run, parsed_params)
+          if authorization_result.has_key?(:denied)
+            result[business_operation_name][agent_object_id] = { errors: { authorization_result[:denied] => '' }}
+            next # authorization guard
+          end
+        end
+
         business_operation.run(*parsed_params).then do |operation_result|
           result[business_operation_name][agent_object_id] = operation_result
         end
--- lib/hyperstack/business/config.rb
+++ lib/hyperstack/business/config.rb
@@ -3,10 +3,12 @@ if RUBY_ENGINE != 'opal'
 
     # available settings
     class << self
+      attr_accessor :business_use_authorization
       attr_accessor :valid_business_class_names
     end
 
     # default values
+    self.business_use_authorization = true
     self.valid_business_class_names = []
   end
 end
