316c2f3aaf4329f5c666a4b0ad609848f6c5ce49
Jan Biedermann <jan@kursator.de>
2018-08-27 22:44:47 +0200

    agent improvement

--- lib/hyperstack/transport/request_agent.rb
+++ lib/hyperstack/transport/request_agent.rb
@@ -5,11 +5,15 @@ module Hyperstack
         @_agents ||= {}
       end
 
+      def self.get(object_id)
+        agents.delete(object_id.to_s)
+      end
+
       attr_accessor :result
       attr_accessor :errors
 
       def intitialize
-        self.class.agents[object_id] = self
+        self.class.agents[object_id.to_s] = self
       end
     end
   end
