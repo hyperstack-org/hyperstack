551e17b02804ac9172c6f1a4d2293cbbaaa18e66
Jan Biedermann <jan@kursator.de>
2018-08-27 22:44:27 +0200

    agent improvement

--- lib/hyperstack/business/class_methods.rb
+++ lib/hyperstack/business/class_methods.rb
@@ -137,7 +137,7 @@ module Hyperstack
 
         def process_response(response)
           response.keys.each do |agent_object_id|
-            agent = Hyperstack::Transport::RequestAgent.agents.delete(agent_object_id)
+            agent = Hyperstack::Transport::RequestAgent.get(agent_object_id)
             agent.result = response[agent_object_id]
           end
         end
