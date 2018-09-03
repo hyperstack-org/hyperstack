fe204e394b162c798ff5ba25d1679c97c951a574
Jan Biedermann <jan@kursator.de>
2018-09-03 04:49:37 +0200

    make sure rails does the right thing

--- ruby/hyper-component/lib/hyper-component.rb
+++ ruby/hyper-component/lib/hyper-component.rb
@@ -37,6 +37,29 @@ else
   require 'hyperstack/config'
   require 'hyperstack/view_helpers'
   Opal.append_path(__dir__.untaint)
+  if defined?(Rails)
+    module Hyperstack
+      module Model
+        class Railtie < Rails::Railtie
+          def delete_first(a, e)
+            a.delete_at(a.index(e) || a.length)
+          end
+
+          config.before_configuration do |_|
+            Rails.configuration.tap do |config|
+              # rails will add everything immediately below app to eager and auto load, so we need to remove it
+              delete_first config.eager_load_paths, "#{config.root}/app/hyperstack"
+
+              unless Rails.env.production?
+                # rails will add everything immediately below app to eager and auto load, so we need to remove it
+                delete_first config.autoload_paths, "#{config.root}/app/hyperstack"
+              end
+            end
+          end
+        end
+      end
+    end
+  end
   if Dir.exist?(File.join('app', 'hyperstack'))
     # Opal.append_path(File.expand_path(File.join('app', 'hyperstack', 'components')))
     Opal.append_path(File.expand_path(File.join('app', 'hyperstack'))) unless Opal.paths.include?(File.expand_path(File.join('app', 'hyperstack')))
