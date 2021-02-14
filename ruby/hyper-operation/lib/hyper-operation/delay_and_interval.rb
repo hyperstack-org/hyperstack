711aa5a40e3d136cc378bbb7755cbe5368c6fcc5
catmando <mitch@catprint.com>
2021-02-13 22:44:23 -0500

    changed how we access after and every on server

--- ruby/hyper-operation/lib/hyper-operation/delay_and_interval.rb
+++
@@ -1,9 +0,0 @@
-module Kernel
-  def every(time, &block)
-    Thread.new { loop { sleep time; block.call }}
-  end
-
-  def after(time, &block)
-    Thread.new { sleep time; block.call }
-  end
-end
