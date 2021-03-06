4dd0fe981b344b7e58c77e52b774ca0470f0a07e
catmando <mitch@catprint.com>
2021-03-06 08:28:50 -0500

    cleaning up travis 5

--- ruby/hyper-spec/lib/hyper-spec.rb
+++ ruby/hyper-spec/lib/hyper-spec.rb
@@ -24,8 +24,6 @@ require 'hyper-spec/expectations'
 
 require 'parser/current'
 require 'webdrivers/chromedriver'
-Webdrivers.logger.level = :DEBUG
-puts 'logger level set to :DEBUG'
 if defined?(Selenium::WebDriver::Firefox)
   require 'selenium/web_driver/firefox/profile'
   require 'webdrivers/geckodriver'
