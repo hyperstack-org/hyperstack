748d88c87f7e6c8ec854f05bf9c140812a0486de
catmando <mitch@catprint.com>
2021-03-16 12:03:41 -0400

    forgot to add new server_side_auto_require.rb file

---
+++ ruby/rails-hyperstack/lib/hyperstack/server_side_auto_require.rb
@@ -0,0 +1,39 @@
+Rails.configuration.autoloader = :classic
+
+module ActiveSupport
+  module Dependencies
+    HYPERSTACK_DIR = "hyperstack"
+    class << self
+      alias original_require_or_load require_or_load
+
+      # before requiring_or_loading a file, first check if
+      # we have the same file in the server side directory
+      # and add that as a dependency
+
+      def require_or_load(file_name, const_path = nil)
+        add_server_side_dependency(file_name)
+        original_require_or_load(file_name, const_path)
+      end
+
+      # search the filename path from the end towards the beginning
+      # for the HYPERSTACK_DIR directory.  If found, remove it from
+      # the filename, and if a ruby file exists at that location then
+      # add it as a dependency
+
+      def add_server_side_dependency(file_name)
+        path = File.expand_path(file_name.chomp(".rb"))
+                   .split(File::SEPARATOR).reverse
+        hs_index = path.find_index(HYPERSTACK_DIR)
+
+        return unless hs_index # no hyperstack directory here
+
+        new_path = (path[0..hs_index - 1] + path[hs_index + 1..-1]).reverse
+        load_path = new_path.join(File::SEPARATOR)
+
+        return unless File.exist? "#{load_path}.rb"
+
+        require_dependency load_path
+      end
+    end
+  end
+end
