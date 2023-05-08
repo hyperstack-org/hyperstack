e118e8c0a3be24ceded83c75f6c659923f34124c
catmando <mitch@catprint.com>
2023-05-08 13:42:45 -0400

    now working with zeitwerk loader

--- ruby/rails-hyperstack/lib/hyperstack/server_side_auto_require.rb
+++ ruby/rails-hyperstack/lib/hyperstack/server_side_auto_require.rb
@@ -1,4 +1,17 @@
-Rails.configuration.autoloader = :classic #:zeitwerk
+# require "hyperstack/server_side_auto_require.rb" in your hyperstack initializer
+# to autoload shadowed server side files that match files
+# in the hyperstack directory
+
+if Rails.configuration.try(:autoloader) == :zeitwerk
+  Rails.autoloaders.each do |loader|
+    loader.on_load do |_cpath, _value, abspath|
+      ActiveSupport::Dependencies.add_server_side_dependency(abspath) do |load_path|
+        loader.send(:log, "Hyperstack loading server side shadowed file: #{load_path}") if loader&.logger
+        require("#{load_path}.rb")
+      end
+    end
+  end
+end
 
 module ActiveSupport
   module Dependencies
@@ -11,7 +24,7 @@ module ActiveSupport
       # and add that as a dependency
 
       def require_or_load(file_name, const_path = nil)
-        add_server_side_dependency(file_name)
+        add_server_side_dependency(file_name) { |load_path| require_dependency load_path }
         original_require_or_load(file_name, const_path)
       end
 
@@ -20,7 +33,7 @@ module ActiveSupport
       # the filename, and if a ruby file exists at that location then
       # add it as a dependency
 
-      def add_server_side_dependency(file_name)
+      def add_server_side_dependency(file_name, loader = nil)
         path = File.expand_path(file_name.chomp(".rb"))
                    .split(File::SEPARATOR).reverse
         hs_index = path.find_index(HYPERSTACK_DIR)
@@ -32,7 +45,7 @@ module ActiveSupport
 
         return unless File.exist? "#{load_path}.rb"
 
-        require_dependency load_path
+        yield load_path
       end
     end
   end
