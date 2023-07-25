2844702658975fdf62cdf45caddc51707e22b734
michail <mpantel@aegean.gr>
2023-07-25 19:42:18 +0300

    1.8.0015

--- ruby/hyper-store/lib/hyperstack/legacy/store/version.rb
+++ ruby/hyper-store/lib/hyperstack/legacy/store/version.rb
@@ -1,7 +1,7 @@
 module Hyperstack
   module Legacy
     module Store
-      VERSION = '1.0.alpha1.8.0014'
+      VERSION = '1.0.alpha1.8.0015'
     end
   end
 end
--- ruby/hyperstack-config/lib/hyperstack/config/version.rb
+++ ruby/hyperstack-config/lib/hyperstack/config/version.rb
@@ -1,5 +1,5 @@
 module Hyperstack
   module Config
-    VERSION = '1.0.alpha1.8.0014'
+    VERSION = '1.0.alpha1.8.0015'
   end
 end
